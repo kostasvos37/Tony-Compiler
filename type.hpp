55752004d498e589a0c1f686384250449502f059
Kostis Andriopoulos <kandrio@arrikto.com>
2021-08-04 20:01:38 +0300

    `type.hpp`: Fix minor bug in printing operator for types

--- type.hpp
+++ type.hpp
@@ -7,7 +7,7 @@
 enum TypeBlock {TYPE_int, TYPE_bool, TYPE_char, TYPE_array, TYPE_list};
 
 class Type {
-public: 
+public:
     Type(TypeBlock current, Type *nested): current_type(current), nested_type(nested) {}
     ~Type() {delete nested_type;};
     TypeBlock get_current_type() {
@@ -26,13 +26,13 @@ bool check_type_equality(Type* type1, Type* type2);
 inline std::ostream& operator<< (std::ostream &out, Type* t) {
   Type* curr = t;
   while (curr != nullptr) {
-    switch (t->get_current_type())
+    switch (curr->get_current_type())
     {
       case TYPE_int: out << "\"int\""; break;
       case TYPE_bool: out << "\"bool\""; break;
       case TYPE_char: out << "\"char\""; break;
-      case TYPE_array: out << "\"array\""; break;
-      case TYPE_list: out << "\"list\""; break;
+      case TYPE_array: out << "array: "; break;
+      case TYPE_list: out << "list: "; break;
       default: out << "\"invalid\""; break;
     }
     curr = curr->get_nested_type();
