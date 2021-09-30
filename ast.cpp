942955cffbaeedf5f25aca15eccf8f5c3aa8c9e4
kostasvos37 <el16435@central.ntua.gr>
2021-09-30 21:20:22 +0300

    added ast.cpp

---
+++ ast.cpp
@@ -0,0 +1,11 @@
+#include "ast.hpp"
+
+llvm::LLVMContext AST::TheContext;
+llvm::IRBuilder<> AST::Builder(TheContext);
+llvm::GlobalVariable *AST::TheVars;
+std::unique_ptr<llvm::Module> AST::TheModule;
+std::map<std::string, llvm::AllocaInst*> AST::NamedValues;
+llvm::Type *AST::i1;
+llvm::Type *AST::i8;
+llvm::Type *AST::i32;
+llvm::Type *AST::i64;
