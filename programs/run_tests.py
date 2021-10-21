8d16c60d0d53daa573b3cd3443ed38721535db77
Konstantinos <konstantinosand@yahoo.gr>
2021-10-21 17:39:02 +0300

    Fix minor bug in test script

--- programs/run_tests.py
+++ programs/run_tests.py
@@ -6,19 +6,21 @@ import subprocess
 path_to_tests = os.path.dirname(os.path.realpath(__file__))
 path_for_correct_tests = os.path.join(path_to_tests, "correct")
 unexpected_errors, expected_correct = [], []
-print("Running correct programs...\n")
+print("\nRunning correct programs...\n")
 for filename in glob.glob(os.path.join(path_for_correct_tests, '*.tony')):
     compile_to_llvm = subprocess.run(
         ['./tony', filename],
         capture_output=True)
     if (compile_to_llvm.returncode != 0):
         print("Test:", filename, "has a semantic/syntactic error...")
+        unexpected_errors.append(filename)
         continue
     create_executable = subprocess.run(
-        ["clang", "out.ll", "lib.a", "-o", "out.o", "-lgc"],
+        ["clang-10", "out.ll", "lib.a", "-o", "out.o", "-lgc"],
         capture_output=True)
     if (create_executable.returncode != 0):
         print("Test:", filename, "has an LLVM error...")
+        unexpected_errors.append(filename)
         continue
     run_executable = subprocess.run(["./out.o"], capture_output=True)
     if run_executable.stdout.decode("utf-8") != "42":
@@ -34,6 +36,9 @@ print("\nTest programs that errored out unexpectedly...")
 for f in unexpected_errors:
     print("  ", os.path.basename(f))
 
+if unexpected_errors == []:
+    print("  ", "None")
+
 print("\n--------------------------------------------------------\n")
 
 path_for_incorrect_tests = os.path.join(path_to_tests, "incorrect")
@@ -47,7 +52,7 @@ for filename in glob.glob(os.path.join(path_for_incorrect_tests, '*.tony')):
         expected_errors.append(filename)
         continue
     create_executable = subprocess.run(
-        ["clang", "out.ll", "lib.a", "-o", "out.o", "-lgc"],
+        ["clang-10", "out.ll", "lib.a", "-o", "out.o", "-lgc"],
         capture_output=True)
     if (create_executable.returncode != 0):
         expected_errors.append(filename)
@@ -61,3 +66,6 @@ for f in expected_errors:
 print("\nTest programs that unexpectedly didn't error out...")
 for f in unexpected_correct:
     print("  ", os.path.basename(f))
+
+if unexpected_correct == []:
+    print("  ", "None", "\n")
